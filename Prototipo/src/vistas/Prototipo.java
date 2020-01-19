955ec5cff327234909313bfdbf2276fc58db50c1
XavProgrammer <axvargas@fiec.espol.edu.ec>
2020-01-19 00:57:31 -0500

    Vista INicio Sesion

--- Prototipo/src/vistas/Prototipo.java
+++ Prototipo/src/vistas/Prototipo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package prototipo;
+package vistas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +19,7 @@ public class Prototipo extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("InicioSesion.fxml"));
         
         Scene scene = new Scene(root);
         
