d82162a16fb612f9fe06a6941d1d6def1d5f0589
enmanuelmag <emmanu2006@hotmail.com>
2020-02-01 20:13:40 -0500

    Conexcion singleton

---
+++ Prototipo/src/conexion/Conexion.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
+/**
+ *
+ * @author Msi
+ */
+public class Conexion {
+    
+    private Session conexion = null;
+    
+    private Session Conexion(){
+        
+        SessionFactory sesion = NewHibernateUtil.getSessionFactory();
+        return sesion.openSession();  
+    }
+    
+    public Session getInstance(){
+        
+        return conexion == null ? conexion = Conexion() : conexion;
+    }
+    
+    public void close(){
+        
+        if(conexion != null && conexion.isOpen()) conexion.close();
+                
+    }
+    
+    public boolean isConnected(){
+        
+        return conexion.isConnected();
+    }
+    
+    
+}
