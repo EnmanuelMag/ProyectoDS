1725f78606fae6930edc5b152325366a6beefa55
axvargas <axvargas@fiec.espol.edu.ec>
2020-02-02 13:58:55 -0500

    form Cliente y form vendedor mejorado

--- Prototipo/src/vistas/FormVendedorController.java
+++ Prototipo/src/vistas/FormVendedorController.java
@@ -9,20 +9,28 @@ import com.jfoenix.controls.JFXButton;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
  *
-
+ *
  * @author Josue
-
+ *
  */
 public class FormVendedorController implements Initializable {
 
@@ -51,7 +59,8 @@ public class FormVendedorController implements Initializable {
     private JFXButton registrarBtn;
     @FXML
     private JFXButton limpiarBtn;
-
+    @FXML
+    private Label xLbl;
 
     /**
      * Initializes the controller class.
@@ -59,10 +68,47 @@ public class FormVendedorController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
-    private void registrarmeHandle(ActionEvent event) {
+    private void registrarmeHandle(ActionEvent event) throws IOException {
+        //Logica de registro
+        Parent root = FXMLLoader.load(getClass().getResource("AdminPrincipal.fxml"));
+
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.show();
+        stackPaneRoot.getScene().getWindow().hide();
     }
-    
+
+    private void limpiarHandle(ActionEvent event) {
+        nombreLbl.setText("");
+        apellidoLbl.setText("");
+        identificacionLbl.setText("");
+        telefonoLbl.setText("");
+        direccionLbl.setText("");
+        correoLbl.setText("");
+        cargoLbl.setText("");
+        telefonoLaboralLbl.setText("");
+    }
+
+    @FXML
+    private void exitHandle(MouseEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("AdminPrincipal.fxml"));
+
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.show();
+        stackPaneRoot.getScene().getWindow().hide();
+    }
+
 }
