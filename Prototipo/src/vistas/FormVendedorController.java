6814c98a7af964aa5c18db1c789cf8f93516a2c4
axvargas <axvargas@fiec.espol.edu.ec>
2020-02-01 19:13:48 -0500

    Cliente y vendedor ids

--- Prototipo/src/vistas/FormVendedorController.java
+++ Prototipo/src/vistas/FormVendedorController.java
@@ -8,26 +8,19 @@ package vistas;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Spinner;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
  *
- * @author Josue
+ * @author Asus
  */
-public class FormCliente1Controller implements Initializable {
+public class FormVendedorController implements Initializable {
 
     @FXML
     private StackPane stackPaneRoot;
@@ -36,27 +29,23 @@ public class FormCliente1Controller implements Initializable {
     @FXML
     private JFXTextField apellidoLbl;
     @FXML
-    private JFXTextField telefLbl;
+    private JFXTextField identificacionLbl;
     @FXML
-    private JFXTextField correoLbl;
+    private JFXTextField telefonoLbl;
     @FXML
     private JFXTextField direccionLbl;
     @FXML
+    private JFXTextField correoLbl;
+    @FXML
     private JFXComboBox<?> estadoCivilCmb;
-    @FXML
-    private JFXTextField empresaLbl;
-    @FXML
-    private JFXTextField direccionLaboralLbl;
     @FXML
     private JFXTextField cargoLbl;
     @FXML
     private JFXTextField telefonoLaboralLbl;
     @FXML
-    private JFXComboBox<?> cargoLaboralCmb;
+    private JFXButton registrarBtn;
     @FXML
-    private Spinner<?> numeroHijosSp;
-    @FXML
-    private JFXButton registrarmeBtn;
+    private JFXButton limpiarBtn;
 
     /**
      * Initializes the controller class.
@@ -67,21 +56,7 @@ public class FormCliente1Controller implements Initializable {
     }    
 
     @FXML
-    private void registrarmeHandle(ActionEvent event) throws IOException {
-        
-        Parent root = FXMLLoader.load(getClass().getResource("InicioSesion.fxml"));
-        
-        Stage stage=new Stage();
-        stage.initStyle(StageStyle.UNDECORATED);
-        
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        
-        stage.show();
-        stackPaneRoot.getScene().getWindow().hide();
-        
+    private void registrarmeHandle(ActionEvent event) {
     }
-    
-    
     
 }
